371678749acd0417f2971f4d3b2b4ead8632d200
HagerYoussef <hageryossef5@gmail.com>
2023-10-31 23:18:31 +0200

    Home Screen

---
+++ lib/on_boarding_screen_model.dart
@@ -0,0 +1,10 @@
+class OnBoardingScreenModel{
+  String title;
+  String imagepath;
+  OnBoardingScreenModel({required this.title, required this.imagepath});
+}
+List<OnBoardingScreenModel>data=[
+  OnBoardingScreenModel(title: 'Home Furniture', imagepath: 'assets/image1.png'),
+  OnBoardingScreenModel(title: 'Office Furniture.', imagepath: 'assets/image2.png'),
+  OnBoardingScreenModel(title: 'Relax Furniture.', imagepath: 'assets/image3.png'),
+];
